959aa155976d101d5baa89906f1590f382a20c3b
rodrigo debian <correo.rodrigo@gmail.com>
2021-03-19 16:01:40 -0600

    progress

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use crate::types::types::first;
 use crate::types::types::json_to_input;
-use crate::types::types::renta;
+use crate::types::types::output_to_json;
+use crate::types::types::to_output;
 use crate::types::types::Input;
 use std::io::{self, BufRead};
 mod types;
@@ -10,5 +11,8 @@ fn main() {
     let inputs: Vec<Input> = stdin.lock().lines().map(json_to_input).collect();
     let item = first(&inputs).unwrap();
 
-    println!("{}", renta(&item))
+    let outputs: Vec<String> = to_output(item).iter().map(output_to_json).collect();
+    for o in outputs {
+        println!("{:?}", o);
+    }
 }
--- src/types.rs
+++ src/types.rs
@@ -33,4 +33,62 @@ pub mod types {
             }
         }
     }
+
+    #[derive(Clone, Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    pub struct Output {
+        pub periodo: i64,
+        pub pago: f64,
+        pub interes: f64,
+        pub abono: f64,
+        pub capital_pagado: f64,
+        pub saldo_insoluto: f64,
+    }
+
+    pub fn to_output(item: &Input) -> Vec<Output> {
+        let renta = renta(item);
+        let mut result = Vec::new();
+        match item {
+            Input::Renta { s, i, m, n } => {
+                let interes_periodo = (i / m) * s;
+                let pago = renta + interes_periodo;
+                let saldo_insoluto = s.clone();
+                let mut interes_acumulado = interes_periodo;
+                let abono_periodo = pago - interes_acumulado;
+                let _capital_pagado = pago - interes_acumulado;
+                let mut abono_acumulado = abono_periodo;
+                let capital_pagado = saldo_insoluto - abono_acumulado;
+                let rounds = ((m * n) + 1.0) as i64;
+                for periodo in 0..rounds {
+                    if periodo == 0 {
+                        result.push(Output {
+                            periodo: periodo,
+                            pago: 0 as f64,
+                            interes: 0 as f64,
+                            abono: 0 as f64,
+                            capital_pagado: 0 as f64,
+                            saldo_insoluto: saldo_insoluto.clone(),
+                        });
+                        continue;
+                    }
+                    result.push(Output {
+                        periodo: periodo,
+                        pago: pago,
+                        interes: interes_acumulado,
+                        abono: abono_periodo,
+                        capital_pagado: capital_pagado,
+                        saldo_insoluto: saldo_insoluto.clone(),
+                    });
+
+                    interes_acumulado = interes_acumulado + interes_periodo;
+                    abono_acumulado = abono_acumulado + abono_periodo;
+                }
+            }
+        }
+        result
+    }
+
+    pub fn output_to_json(output: &Output) -> String {
+        serde_json::to_string(output).unwrap()
+    }
 }
