8537c4b99f9f5153ac7a5830e28776389fa89466
rodrigo debian <correo.rodrigo@gmail.com>
2021-03-19 21:30:15 -0600

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use crate::types::types::first;
 use crate::types::types::json_to_input;
 use crate::types::types::to_output;
-use crate::types::types::Input;
 use crate::types::types::Output;
+use crate::types::types::Renta;
 use std::io::{self, BufRead};
 mod types;
 
 fn main() {
     let stdin = io::stdin();
-    let inputs: Vec<Input> = stdin.lock().lines().map(json_to_input).collect();
+    let inputs: Vec<Renta> = stdin.lock().lines().map(json_to_input).collect();
     let item = first(&inputs).unwrap();
 
     println!("PERIODO,PAGO,INTERES,ABONO,CAPITAL PAGADO,SALDO INSOLUTO");
--- src/types.rs
+++ src/types.rs
@@ -3,29 +3,28 @@ pub mod types {
     use std::io::{self};
 
     #[derive(Clone, Serialize, Deserialize, Debug)]
-    #[serde(untagged)]
-    pub enum Input {
-        #[serde(rename_all = "camelCase")]
-        Renta { s: f64, i: f64, m: f64, n: f64 },
+    #[serde(rename_all = "camelCase")]
+    pub struct Renta {
+        s: f64,
+        i: f64,
+        m: f64,
+        n: f64,
     }
 
-    impl crate::Input {
+    impl Renta {
         fn calculate(&self) -> f64 {
-            match &self {
-                Input::Renta { s, i, m, n } => {
-                    let base = 1.0 + (i / m);
-                    let exponente = m * n;
-                    let arriba = f64::powf(base, exponente) - 1.0;
-                    let r = s / (arriba / (i / m));
-                    r
-                }
-            }
+            let Renta { s, i, m, n } = &self;
+            let base = 1.0 + (i / m);
+            let exponente = m * n;
+            let arriba = f64::powf(base, exponente) - 1.0;
+            let r = s / (arriba / (i / m));
+            r
         }
     }
 
-    pub fn json_to_input(line: io::Result<String>) -> Input {
+    pub fn json_to_input(line: io::Result<String>) -> Renta {
         let json = &line.unwrap();
-        let deserialized: Input = serde_json::from_str(json).unwrap();
+        let deserialized: Renta = serde_json::from_str(json).unwrap();
         deserialized
     }
 
@@ -43,57 +42,54 @@ pub mod types {
         pub saldo_insoluto: f64,
     }
 
-    pub fn to_output(item: &Input) -> Vec<Output> {
+    pub fn to_output(item: &Renta) -> Vec<Output> {
         let renta = item.calculate();
         let mut result = Vec::new();
-        match item {
-            Input::Renta { s, i, m, n } => {
-                let rounds = ((m * n) + 1.0) as i64;
-                let interes_periodo = i / m;
-                let pago = renta + (interes_periodo * s);
-                let mut saldo_insoluto = s.clone();
-                let mut capital_pagado = 0.0;
+        let Renta { s, i, m, n } = item;
+        let rounds = ((m * n) + 1.0) as i64;
+        let interes_periodo = i / m;
+        let pago = renta + (interes_periodo * s);
+        let mut saldo_insoluto = s.clone();
+        let mut capital_pagado = 0.0;
 
-                for periodo in 0..rounds {
-                    if periodo == 0 {
-                        result.push(Output {
-                            periodo: periodo,
-                            pago: 0 as f64,
-                            interes: 0 as f64,
-                            abono: 0 as f64,
-                            capital_pagado: 0 as f64,
-                            saldo_insoluto: saldo_insoluto.clone(),
-                        });
-                        continue;
-                    }
-
-                    if periodo == 1 {
-                        saldo_insoluto = saldo_insoluto - renta;
-                        capital_pagado = renta;
-                        result.push(Output {
-                            periodo: periodo,
-                            pago: pago as f64,
-                            interes: interes_periodo * s,
-                            abono: renta as f64,
-                            capital_pagado: capital_pagado,
-                            saldo_insoluto: saldo_insoluto,
-                        });
-                        continue;
-                    }
-                    let interes = interes_periodo * saldo_insoluto;
-                    let abono = pago - interes;
-                    capital_pagado = capital_pagado + abono;
-                    saldo_insoluto = saldo_insoluto - abono;
-                    result.push(Output {
-                        periodo: periodo,
-                        pago: pago,
-                        interes: interes,
-                        abono: abono,
-                        capital_pagado: capital_pagado,
-                        saldo_insoluto: saldo_insoluto,
-                    });
-                }
+        for periodo in 0..rounds {
+            if periodo == 0 {
+                result.push(Output {
+                    periodo: periodo,
+                    pago: 0 as f64,
+                    interes: 0 as f64,
+                    abono: 0 as f64,
+                    capital_pagado: 0 as f64,
+                    saldo_insoluto: saldo_insoluto.clone(),
+                });
+                continue;
             }
+
+            if periodo == 1 {
+                saldo_insoluto = saldo_insoluto - renta;
+                capital_pagado = renta;
+                result.push(Output {
+                    periodo: periodo,
+                    pago: pago as f64,
+                    interes: interes_periodo * s,
+                    abono: renta as f64,
+                    capital_pagado: capital_pagado,
+                    saldo_insoluto: saldo_insoluto,
+                });
+                continue;
+            }
+            let interes = interes_periodo * saldo_insoluto;
+            let abono = pago - interes;
+            capital_pagado = capital_pagado + abono;
+            saldo_insoluto = saldo_insoluto - abono;
+            result.push(Output {
+                periodo: periodo,
+                pago: pago,
+                interes: interes,
+                abono: abono,
+                capital_pagado: capital_pagado,
+                saldo_insoluto: saldo_insoluto,
+            });
         }
         result
     }
