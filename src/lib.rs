b11b07865ca66151b209758f01b9905e5197ce8b
rodrigo debian <correo.rodrigo@gmail.com>
2021-03-24 16:33:17 -0600

    refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -23,16 +23,6 @@ pub mod types {
         m: f64,
     }
 
-    impl Renta {
-        fn calculate(&self) -> f64 {
-            let Renta { s, i, m, a } = &self;
-            let base = 1.0 + (i / m);
-            let exponente = m * a;
-            let arriba = f64::powf(base, exponente) - 1.0;
-            s / (arriba / (i / m))
-        }
-    }
-
     pub struct Output {
         pub periodo: i64,
         pub pago: f64,
@@ -42,65 +32,74 @@ pub mod types {
         pub saldo_insoluto: f64,
     }
 
-    pub fn to_output(item: &Renta) -> Vec<Output> {
-        let renta = item.calculate();
-        let mut result = Vec::new();
-        let Renta { s, i, m, a } = item;
-        let rounds = ((m * a) + 1.0) as i64;
-        let interes_periodo = i / m;
-        let pago = renta + (interes_periodo * s);
-        let mut saldo_insoluto = *s;
-        let mut capital_pagado = 0.0;
-
-        for periodo in 0..rounds {
-            let o = match periodo {
-                0 => Output {
-                    periodo,
-                    pago: 0 as f64,
-                    interes: 0 as f64,
-                    abono: 0 as f64,
-                    capital_pagado: 0 as f64,
-                    saldo_insoluto,
-                },
-                1 => {
-                    saldo_insoluto -= renta;
-                    capital_pagado = renta;
-                    Output {
-                        periodo,
-                        pago,
-                        interes: interes_periodo * s,
-                        abono: renta as f64,
-                        capital_pagado,
-                        saldo_insoluto,
-                    }
-                }
-                _ => {
-                    let interes = interes_periodo * saldo_insoluto;
-                    let abono = pago - interes;
-                    capital_pagado += abono;
-                    saldo_insoluto -= abono;
-                    Output {
-                        periodo,
-                        pago,
-                        interes,
-                        abono,
-                        capital_pagado,
-                        saldo_insoluto,
-                    }
-                }
-            };
-            result.push(o);
+    impl Renta {
+        pub fn calculate(&self) -> f64 {
+            let Renta { s, i, m, a } = &self;
+            let base = 1.0 + (i / m);
+            let exponente = m * a;
+            let arriba = f64::powf(base, exponente) - 1.0;
+            s / (arriba / (i / m))
         }
-        result
-    }
 
-    pub fn to_csv(outputs: Vec<Output>) {
-        println!("PERIODO,PAGO,INTERES,ABONO,CAPITAL PAGADO,SALDO INSOLUTO");
-        for o in outputs {
-            println!(
-                "{:?},{:.2},{:.2},{:.2},{:.2},{:.2}",
-                o.periodo, o.pago, o.interes, o.abono, o.capital_pagado, o.saldo_insoluto
-            );
+        pub fn to_output(&self) -> Vec<Output> {
+            let Renta { s, i, m, a } = &self;
+            let renta = *&self.calculate();
+            let mut result = Vec::new();
+            let rounds = ((m * a) + 1.0) as i64;
+            let interes_periodo = i / m;
+            let pago = renta + (interes_periodo * s);
+            let mut saldo_insoluto = *s;
+            let mut capital_pagado = 0.0;
+            for periodo in 0..rounds {
+                let o = match periodo {
+                    0 => Output {
+                        periodo,
+                        pago: 0 as f64,
+                        interes: 0 as f64,
+                        abono: 0 as f64,
+                        capital_pagado: 0 as f64,
+                        saldo_insoluto,
+                    },
+                    1 => {
+                        saldo_insoluto -= renta;
+                        capital_pagado = renta;
+                        Output {
+                            periodo,
+                            pago,
+                            interes: interes_periodo * s,
+                            abono: renta as f64,
+                            capital_pagado,
+                            saldo_insoluto,
+                        }
+                    }
+                    _ => {
+                        let interes = interes_periodo * saldo_insoluto;
+                        let abono = pago - interes;
+                        capital_pagado += abono;
+                        saldo_insoluto -= abono;
+                        Output {
+                            periodo,
+                            pago,
+                            interes,
+                            abono,
+                            capital_pagado,
+                            saldo_insoluto,
+                        }
+                    }
+                };
+                result.push(o);
+            }
+            result
+        }
+
+        pub fn to_csv(&self, outputs: Vec<Output>) {
+            println!("PERIODO,PAGO,INTERES,ABONO,CAPITAL PAGADO,SALDO INSOLUTO");
+            for o in outputs {
+                println!(
+                    "{:?},{:.2},{:.2},{:.2},{:.2},{:.2}",
+                    o.periodo, o.pago, o.interes, o.abono, o.capital_pagado, o.saldo_insoluto
+                );
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-use crate::lib::types::to_csv;
-use crate::lib::types::to_output;
 use crate::lib::types::Renta;
 use clap::Clap;
 mod lib;
 
 fn main() {
     let input: Renta = Renta::parse();
-    to_csv(to_output(&input));
+    input.to_csv(input.to_output());
 }
